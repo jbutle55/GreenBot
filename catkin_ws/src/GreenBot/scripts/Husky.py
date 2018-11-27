926f583af5a514315a06de51953a13149c0d73b2
jbutle55 <jbutle55@uwo.ca>
2018-11-27 10:14:50 -0500

    Reorganized GreenBot class

--- catkin_ws/src/GreenBot/scripts/Husky.py
+++ catkin_ws/src/GreenBot/scripts/Husky.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import sys
-import os
+
+import sys, os
 import husky_msgs
 import rospy
 import roslaunch
@@ -9,6 +9,12 @@ import theora_image_transport
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 import twist_mux_msgs
+import argparse
+
+# Should this be top import?
+import roslib
+roslib.load_manifest("GreenBot")
+
 
 # NODES
 # ---------------------------------------
@@ -20,55 +26,46 @@ import twist_mux_msgs
 # Front camera Node
 
 
-def launch():  # Launch the GreenBot process
-    # TODO does the launch file negate this function?
-    # See: http://wiki.ros.org/roslaunch/API%20Usage
-    print 'Hello World'
-    package = 'GreenBot'
-    executable = 'GreenBot'
-    node = roslaunch.core.Node(package, executable)
-    print 'Hello World'
-    greenbot = roslaunch.scriptapi.ROSLaunch()
-    greenbot.start()
-    print 'Hello World'
-    greenbot_process = greenbot.launch(node)
-    print greenbot_process.is_alive()
-
-    return greenbot_process
-
-
-def land(greenbot_process):  # Kill the GreenBot process
-    # TODO Might need to included in same function as launch
-    greenbot_process.stop()
-    return
-
-
+# ------------------------------------------------------------------------------
 class GreenBot:
     """A node for the Husky A200
     Subscribe to mux, QR code detection, and rangefinder sensors
     Publish velocity commands
     """
 
-    def __init__(self):
-        global vel_cmd
+    def __init__(self, config):
+        self.__dict__.update(config)
+        self.detection_timeout = rospy.Duration(self.detection_timeout)
+        self.setup_ros_node()
+
+        # Publish on husky_velocity_controller/cmd_vel topic
+        self.control_pub = rospy.Publisher('husky_velocity_controller/cmd_vel', Twist)
+
+        return
+
+# ------------------------------------------------------------------------------
+    def setup_ros_node(self):
         rospy.init_node('Husky', anonymous=True)  # Create the Husky node
 
         # Subscriber initiations
-        self.sub_controller()
-        self.sub_prox_sensor()
-        self.sub_mux()
+        self.subscribe_node("controller")
+        self.subscribe_node("mux")
+        self.subscribe_node("prox")
+        self.subscribe_node("vid")
 
-        # Publisher initiations
-        self.pub_cmd_vel()
-        
-        rospy.spin()
         return
 
-    # TODO I think this and the callback can be removed b/c sub_mux replaces it
-    # Subscriber to joystick input commands
-    def sub_controller(self):
-        # Receive control input from joystick Node
-        rospy.Subscriber("joystick", Joy, self.callback_controller)
+# ------------------------------------------------------------------------------
+    def subscribe_node(self, target):
+        if target is "controller":  # Does sub mux replace this?
+            rospy.Subscriber(self.controller_topic, Joy, self.callback_controller)
+        elif target is "prox":
+            # TODO Create proximity sensor sub once using sensors
+            pass
+        elif target is "mux":
+            rospy.Subscriber('cmd_vel', Twist, self.callback_mux)
+        elif target is "vid":
+            rospy.Subscriber()
         return
 
     @staticmethod
@@ -81,12 +78,6 @@ class GreenBot:
         twist.linear.x = 4 * data.axes[1]  # Left stick vertical
         twist.angular.z = 4 * data.axes[0]  # Left stick horizontal
 
-        # Publish w. velocity command publisher
-        vel_cmd.publish(twist)
-        return
-
-    def sub_mux(self):
-        rospy.Subscriber('cmd_vel', Twist, self.callback_mux)
         return
 
     @staticmethod
@@ -100,18 +91,9 @@ class GreenBot:
 
         return mux_twist
 
-    def sub_prox_sensor(self):
-        rospy.Subscriber("prox1", Twist, self.callback_prox)  # TODO probably not Twist
-
-        return
-
     @staticmethod
     def callback_prox(data):
-        if len(data) == 0:
-            return  # No sensor input
-
-        twist = Twist()
-        # TODO data here then publish
+        pass
         return
 
     # Publisher of velocity commands to the GreenBot motor system
@@ -121,12 +103,55 @@ class GreenBot:
         rospy.Publisher('husky_velocity_controller/cmd_vel', Twist)
         return
 
+# ------------------------------------------------------------------------------
+    def publish_move(self, linear, angular):
+        
+        return
+
+# ------------------------------------------------------------------------------
+    def compute_vel(self):
+        linear_cmd, angular_cmd = 0, 0
+
+        return linear_cmd, angular_cmd
+
+# ------------------------------------------------------------------------------
     def main(self):
+        rate = rospy.Rate(self.control_rate)
+        while not rospy.is_shutdown():
+            linear, angular = self.compute_vel()
+
+            if linear != 0 or angular != 0:
+                self.publish_move(linear, angular)
+
+        rate.sleep()
 
         return
 
 
-if __name__ == '__main__':
-    green = GreenBot()
+def run_greenbot(args):
+    # Standard config dict for both operation modes
+    config = dict(vel_topic=rospy.get_param("~cmd_vel_topic", "husky/plan_cmd_vel"),
+                  drive_rate=rospy.get_param("~drive_rate", 1.0),
+                  turn_rate=rospy.get_param("~turn_rate", 0.5),
+                  control_rate=rospy.get_param("~control_rate", 30),  # 30Hz publish rate
+                  controller_topic=rospy.get_param("joystick"))  # ~?
+
+    if args.mode is 'tele':
+        # Add dict keys relevant to teleoperation mode
+        pass
+    else:
+        # Add dict keys relevant to autonomous mode
+        pass
+    green = GreenBot(config)
     green.main()
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Determine the mode the GreenBot should start in.')
+    parser.add_argument('mode', choices=['tele', 'auto'],
+                        help="Enter 'Tele' to run GreenBot in teleoperation mode. Enter 'auto' for"
+                             "the GreenBot to autonomously patrol the greenhouse.")
+    arguments = parser.parse_args()
+    sys.exit(run_greenbot(arguments))
 
